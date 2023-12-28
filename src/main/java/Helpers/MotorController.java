4afacdfc6fbdc2dccb8956466613ee5a66ac005f
pancakecylinder <suhitm8@gmail.com>
2023-12-28 11:24:35 +0000

    Helper Class MotorController.java

--- src/main/java/Helpers/MotorController.java
+++ src/main/java/Helpers/MotorController.java
@@ -1,14 +1,22 @@
 
+package Helpers;
 
-
-
+import java.io.IOException;
 
 import se.hirt.pi.adafruit.pwm.PWMDevice;
+import se.hirt.pi.adafruit.pwm.PWMDevice.PWMChannel;
 
 public class MotorController extends PWMDevice {
 	
-	public void Test(){
-		
-		System.out.println("hi");
+PWMDevice name = new PWMDevice();
+
+	public MotorController(int channelnumber) throws IOException{
+		PWMChannel channel = name.getChannel(channelnumber);
 	}
+	
+	public void set(double frequency) throws IOException{
+		name.setPWMFreqency(frequency);
+	}
+	
+
 }
