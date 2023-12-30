d67b3c036054c673571973317ccb7686f4a23af3
pancakecylinder <suhitm8@gmail.com>
2023-12-30 00:10:12 +0000

    Test program finally works!!

--- src/main/java/robot/MotorTest.java
+++ src/main/java/robot/MotorTest.java
@@ -3,9 +3,11 @@ package robot;
 import robot.Helpers.*;
 import java.io.IOException;
 
-import se.hirt.pi.adafruit.pwm.PWMDevice.*;
+import se.hirt.pi.adafruit.pwm.PWMDevice;
+import se.hirt.pi.adafruit.pwm.PWMDevice.PWMChannel;
 
 public class MotorTest {
+	
 	public static void main(String[] args) {
 		try {
 		MotorController.initializeVictorSP();
@@ -14,16 +16,21 @@ public class MotorTest {
 	}
 		try {
 		MotorController motor1 = new MotorController(0);
+		for(i=1; i < 11; i++){
 		motor1.setVictorSP(1);
-
+		Thread.sleep(5000);
 		motor1.setVictorSP(-1);
+		Thread.sleep(5000);
+	}
 
 	} catch (IOException e) {
 		System.out.println("MotorController could not be made!");
+	} catch (InterruptedException a) {
+		System.out.println("Not a valid time");
 	}
+	
+	System.exit(0);
 		
-		
-		System.exit(0);
 		
 		
 	}
